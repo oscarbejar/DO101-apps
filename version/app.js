0be984bae51e2d01fd230a106bc65d5ddcb1bd19
Oscar Bejar <oskrbejar@gmail.com>
2020-05-09 16:29:44 -0300

    change version 2

--- version/app.js
+++ version/app.js
@@ -5,7 +5,8 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    //change to version 2
+    response = 'This is version 2 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
