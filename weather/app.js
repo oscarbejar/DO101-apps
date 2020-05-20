5b21fdf0a003c895e9daa23146fe63d735d40732
oskrbejar <55465529+oscarbejar@users.noreply.github.com>
2020-05-20 01:16:19 -0300

    commit

--- weather/app.js
+++ weather/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+// view engine setup2
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
